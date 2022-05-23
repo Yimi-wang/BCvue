d04ec9c4da4acda9d1f9bf268b5ea69933192719
Yimi-wang <wmt200802@aliyun.com>
2022-05-23 14:12:06 +0200

    01

--- bridgechinoise-New Version/bridgechinoise/src/Bridgechinoise.java
+++ bridgechinoise-New Version/bridgechinoise/src/Bridgechinoise.java
@@ -8,6 +8,7 @@ public class Bridgechinoise {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         GameProcess gameProcess = new GameProcess();
         gameProcess.creatJeu();
+
     }
 
 
--- bridgechinoise-New Version/bridgechinoise/src/Vue/Start.java
+++ bridgechinoise-New Version/bridgechinoise/src/Vue/Start.java
@@ -3,6 +3,7 @@ package Vue;
 public class Start {
 
     public static class Starter {
+        //基础界面，准备删除
         public static void main(String[] args) {
             Vue.InterfaceInitialise.start();
         }
