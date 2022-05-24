f28bf8f3513b0d2c9365285dcaaa91e3da61492a
Cai Feng Lin <15213432067@163.com>
2022-05-24 12:35:37 +0800

    swing添加主界面

---
+++ bridgechinoise-New Version/bridgechinoise/src/Vue/IconOnButton.java
@@ -0,0 +1,40 @@
+package Vue;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+/**
+ * 自定义图片按钮
+ * @author lenovo
+ *
+ */
+public class IconOnButton {
+    //声明窗体
+    private JFrame frame = null;
+    //获取按钮方法
+    public void getButtonss(){
+        JFrame frame=new JFrame();
+        String path="D:\\OutSourcing\\CardGame\\BCvue\\bridgechinoise-New Version\\bridgechinoise\\src\\images\\start.png";
+        ImageIcon icon1=new ImageIcon(path);
+        Image img = icon1.getImage();
+        Image newimg = img.getScaledInstance(120,30,java.awt.Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(newimg);
+        System.out.println(icon.getIconHeight());
+        System.out.println(icon.getIconWidth());
+        JButton button=new JButton(icon);
+        button.setBounds(10,10,120,40);
+        //JButton button=new JButton("Click Me!");//new出一个按钮的对象
+        frame.add(button);//向容器加入组件
+        frame.setSize(600,300);
+        frame.setLocation(300,200);
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args){
+        IconOnButton iob = new IconOnButton();
+        iob.getButtonss();
+    }
+
+}
---
+++ bridgechinoise-New Version/bridgechinoise/src/Vue/Main.java
@@ -0,0 +1,136 @@
+package Vue;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class Main extends JFrame {
+
+
+//    public Main()
+//    {
+//        setTitle("");    //设置显示窗口标题
+//        setSize(600,400);    //设置窗口显示尺寸
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //置窗口是否可以关闭
+//        JLabel jl=new JLabel("这是使用JFrame类创建的窗口");    //创建一个标签
+//        URL resource = this.getClass().getResource("../images/MainMenu.png");
+//        Container c=getContentPane();    //获取当前窗口的内容窗格
+//        c.add(jl);    //将标签组件添加到内容窗格上
+//        setVisible(true);    //设置窗口是否可见
+//    }
+//    public static void main(String[] agrs)
+//    {
+//        new Main();    //创建一个实例化对象
+//    }
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Main window = new Main();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public Main() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        // 初始化窗体
+        frame = new JFrame("Bcvue game");
+        frame.setBounds(300, 300, 720, 506);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        // 游戏文本标题
+        JLabel jLabel = new JLabel("Bridge Chinois");
+        jLabel.setBounds(20, 15, 300, 30);
+        Font f = new Font("隶书",Font.PLAIN,30);
+        jLabel.setFont(f);
+        Color fg = new Color(255,255,255);
+        jLabel.setForeground(fg);
+        frame.getContentPane().add(jLabel);
+
+        // start按钮
+        //加载背景图片
+        String startPath="D:\\OutSourcing\\CardGame\\BCvue\\bridgechinoise-New Version\\bridgechinoise\\src\\images\\start.png";
+        ImageIcon startIcon1=new ImageIcon(startPath);
+        Image startImg = startIcon1.getImage();
+        Image startNewimg = startImg.getScaledInstance(120,40,java.awt.Image.SCALE_SMOOTH);
+        ImageIcon startIcon2 = new ImageIcon(startNewimg);
+
+        JButton btnStartButton = new JButton(startIcon2);
+        btnStartButton.setBounds(20, 80, 120, 40);
+        frame.getContentPane().add(btnStartButton);
+
+
+        // online
+        //加载背景图片
+        String onlinePath="D:\\OutSourcing\\CardGame\\BCvue\\bridgechinoise-New Version\\bridgechinoise\\src\\images\\online.png";
+        ImageIcon onlineIcon1=new ImageIcon(onlinePath);
+        Image onlineImg = onlineIcon1.getImage();
+        Image onlineNewImg = onlineImg.getScaledInstance(120,40,java.awt.Image.SCALE_SMOOTH);
+        ImageIcon onlineIcon2 = new ImageIcon(onlineNewImg);
+
+        JButton btnOnlineButton = new JButton(onlineIcon2);
+        btnOnlineButton.setBounds(20, 140, 120, 40);
+        frame.getContentPane().add(btnOnlineButton);
+
+
+        // load
+        //加载背景图片
+        String loadPath="D:\\OutSourcing\\CardGame\\BCvue\\bridgechinoise-New Version\\bridgechinoise\\src\\images\\load.png";
+        ImageIcon loadIcon1=new ImageIcon(loadPath);
+        Image loadImg = loadIcon1.getImage();
+        Image loadNewImg = loadImg.getScaledInstance(120,40,java.awt.Image.SCALE_SMOOTH);
+        ImageIcon loadIcon2 = new ImageIcon(loadNewImg);
+
+        JButton btnLoadButton = new JButton(loadIcon2);
+        btnLoadButton.setBounds(20, 200, 120, 40);
+        frame.getContentPane().add(btnLoadButton);
+
+
+
+        // load
+        //加载背景图片
+        String settingPath="D:\\OutSourcing\\CardGame\\BCvue\\bridgechinoise-New Version\\bridgechinoise\\src\\images\\setting.png";
+        ImageIcon settingIcon1=new ImageIcon(settingPath);
+        Image settingImg = settingIcon1.getImage();
+        Image settingNewImg = settingImg.getScaledInstance(120,40,java.awt.Image.SCALE_SMOOTH);
+        ImageIcon settingIcon2 = new ImageIcon(settingNewImg);
+
+        JButton btnSettingButton = new JButton(settingIcon2);
+        btnSettingButton.setBounds(20, 260, 120, 40);
+        frame.getContentPane().add(btnSettingButton);
+
+
+
+
+
+
+
+        // 设置背景
+        JLabel lblBackground = new JLabel(); // 创建一个标签组件对象
+        URL resource = this.getClass().getResource("../images/MainMenu.png"); // 获取背景图片路径
+        ImageIcon icon = new ImageIcon(resource); // 创建背景图片对象
+        lblBackground.setIcon(icon); // 设置标签组件要显示的图标
+        lblBackground.setBounds(0, 0, icon.getIconWidth(), icon.getIconHeight()); // 设置组件的显示位置及大小
+        frame.getContentPane().add(lblBackground); // 将组件添加到面板中
+    }
+}
